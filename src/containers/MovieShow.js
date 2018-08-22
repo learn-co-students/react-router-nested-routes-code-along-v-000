76520a602e47cd6bab704db9aa69a696e93171c9
Raiet <raietsaeed@gmail.com>
2018-08-22 01:00:20 +0000

    Done.

--- src/containers/MovieShow.js
+++ src/containers/MovieShow.js
@@ -1,12 +1,24 @@
 import React from 'react';
  
-const MovieShow = props => {
+ // Note : Just renders the below h3
+// const MovieShow = props => {
  
+//   return (
+//     <div>
+//       <h3>Movies Show Component!</h3>
+//     </div>
+//   );
+// }
+ 
+// Note : Extracts the "match" (current url) and "movies" from props
+const MovieShow = ({match, movies}) => {
   return (
     <div>
-      <h3>Movies Show Component!</h3>
+    
+    {/* Note : Extracting the movieId param from the "match" object to show only the relevant movie info */}
+      <h3>{ movies[match.params.movieId].title }</h3>
     </div>
   );
 }
- 
+
 export default MovieShow;
