3366282b3df17e39fc3a2f991b97e46be1843269
github username <github email address>
2018-08-23 11:36:38 -0400

    Done.

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -2,13 +2,18 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import MoviesList from '../components/MoviesList';
-import MovieShow from './MovieShow';
+import MovieShow from '../components/MovieShow';
 
+// the function name "MoviesPage" also is usually the name of the .js file
 const MoviesPage = ({ match, movies }) => (
   <div>
     <MoviesList movies={movies} />
-  </div>;
-
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a Movie from the list.</h3>
+    )}/>
+     <Route path={`${match.url}/:movieId`} render={routerProps =>
+  <MovieShow movies={movies} {...routerProps} /> }/>
+  </div>
 )
 
 export default MoviesPage
