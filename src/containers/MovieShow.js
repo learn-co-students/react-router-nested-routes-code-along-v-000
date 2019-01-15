2b076883744ba4ef72659751736925e049939c9c
pcherian10 <pcherian10@gmail.com>
2019-01-15 15:40:28 -0500

    Done.

---
+++ src/containers/MovieShow.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+
+const MovieShow = ({match, movies}) => {
+
+    return (
+      <div>
+       <h3>{ movies[match.params.movieID].title }</h3>
+      </div>
+    );
+
+}
+
+export default MovieShow;
