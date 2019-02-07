236f56d7b8cbb8b6e5be0f8e6c1b4345bac5390a
julianjohannesen <github email address>
2019-02-07 16:45:15 -0500

    Done.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,32 +1,42 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import MoviesPage from './MoviesPage';
 
 class App extends Component {
 
-  state = {
-    movies: {
-      1: { id: 1, title: 'A River Runs Through It' },
-      2: { id: 2, title: 'Se7en' },
-      3: { id: 3, title: 'Inception' }
-    }
-  }
+	state = {
+		movies: {
+			1: { id: 1, title: 'A River Runs Through It' },
+			2: { id: 2, title: 'Se7en' },
+			3: { id: 3, title: 'Inception' }
+		}
+	}
 
-  render() {
-    return (
-      <Router>
-        <div>
-          <NavBar />
-          <Route exact path="/" render={() => <div>Home</div>} />
-          <Route path='/movies' render={routerProps => <MoviesPage {...routerProps} movies={this.state.movies}/>} />
-        </div>
-      </Router>
-    );
-  }
+	render() {
+		// We use the render function rather than component and pass
+		// in routerProps which we then pass down to <MoviesPage/> as
+		// its props. We're destructing those props in {...routerProps}.
+		// We also pass in our movie data
+		// QUESTION: What does the presence of "exact" on 32 do here?
+		return (
+			<Router>
+				<div>
+					<NavBar />
+					<Route
+						exact
+						path="/"
+						render={() => <div>Home</div>}
+					/>
+					<Route
+						
+						path='/movies'
+						render={routerProps => <MoviesPage {...routerProps} movies={this.state.movies} />}
+					/>
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -4,10 +4,23 @@ import { Route } from 'react-router-dom';
 import MoviesList from '../components/MoviesList';
 import MovieShow from './MovieShow';
 
+// match is a prop of any route, so it's passed down to us from app as
+// one of our route props. That match property (or is it a method) has a url property that
+// we can use in our path when rendering "Please select a movie" and 
+// in our path when rendering MovieShow 
 const MoviesPage = ({ match, movies }) => (
   <div>
     <MoviesList movies={movies} />
-  </div>;
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a Movie from the list.</h3>
+    )}/>
+    <Route 
+      path={`${match.url}/:movieId`} 
+      render={routerProps => 
+        <MovieShow movies={movies} {...routerProps} />
+      }
+    />
+  </div>
 
 )
 
