25678d322ecf1399cd773803c72d579e3773d26c
github username <github email address>
2019-06-27 18:23:11 -0500

    Done.

--- src/containers/App.js
+++ src/containers/App.js
@@ -10,9 +10,10 @@ class App extends Component {
 
   state = {
     movies: {
-      1: { id: 1, title: 'A River Runs Through It' },
-      2: { id: 2, title: 'Se7en' },
-      3: { id: 3, title: 'Inception' }
+
+      1: { id: 1, title: 'A River Runs Through It  ' },
+      2: { id: 2, title: 'Se7en   ' },
+      3: { id: 3, title: 'Inception   ' }
     }
   }
 
