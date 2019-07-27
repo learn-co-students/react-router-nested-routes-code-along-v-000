0f5dbe9cfa423d4158936204f5c07656710866b7
MargaritaKhodzka <mkhodzka@gmail.com>
2019-07-27 00:24:36 -0400

    MovieShow component and 2 Routes in Movies page to link each movie to its show page

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -6,7 +6,11 @@ import MovieShow from './MovieShow';
 
 const MoviesPage = ({ match, movies }) => (
   <div>
-    <MoviesList movies={movies} />
+    <MoviesList movies = {movies} />
+    <Route exact path = {match.url} render = {() => (
+        <h3>Please select a Movie from the list.</h3>
+      )} />
+    <Route path = {`${match.url}/:movieId`} component = {MovieShow} />
   </div>
 )
 
